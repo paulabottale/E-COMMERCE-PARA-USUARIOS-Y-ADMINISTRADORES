a7b23fa8ed069f99b5180bc37000e4136db66fa7
paulabottale.dev <paulabottale.dev@gmail.com>
2025-06-30 01:52:34 -0300

    creacion de la ruta para la busqueda de productos y exponerla en las rutas

--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productControllers"
+import { createProduct, deleteProduct, getAllProducts, updateProduct, searchProduct } from "../controllers/productControllers"
 import { authMiddleware } from "../middleware/authMiddleware"
 
 const productRouter = Router()
 
 productRouter.get("/", getAllProducts)
+productRouter.get("/search", searchProduct)
 productRouter.post("/", authMiddleware, createProduct)
 productRouter.delete("/:id", authMiddleware, deleteProduct)
 productRouter.patch("/:id", authMiddleware, updateProduct)
